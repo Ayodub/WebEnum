05d5ea26b4a55914e1f2f473e98c5df40cb48082
Ayodub <66164546+Ayodub@users.noreply.github.com>
2021-02-11 14:27:11 +1100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+from PyQt5 import QtWidgets
+import sys
+
+from gui.controllers.splash_screen_controller import SplashScreenController
+
+if __name__=="__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = QtWidgets.QWidget()
+    ui = SplashScreenController()
+    ui.show()
+    sys.exit(app.exec_())
+
